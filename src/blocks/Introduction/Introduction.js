f3a335c923cebfc7fd69da75e8fee49b627652ed
TKreateDev <tanbir98@hotmail.it>
2022-07-14 13:08:11 +0100

    cool gsap on off opacity animation

--- src/blocks/Introduction/Introduction.js
+++ src/blocks/Introduction/Introduction.js
@@ -1,6 +1,23 @@
-import Styles from './styles'
+import Styles from './styles';
+import React from 'react';
+import gsap from 'gsap';
 export default function Introduction() {
-    return(<Styles.Wrapper>
-        <Styles.Title>Hello</Styles.Title>
-    </Styles.Wrapper>)
-}
+    const [text, setText] = React.useState('hello');
+    const wrapper = React.useRef();
+    const title = React.useRef();
+
+    const CustomAnimationEvent = () => {
+        gsap.to(title.current, {opacity: 0, duration: 1, onComplete: () => {
+            setText("bye")
+            gsap.to(title.current, {opacity: 1, duration: 1})
+        }})
+        // gsap.from(title.current, {opacity: 1, duration: 1})
+    }
+
+    return (
+        <Styles.Wrapper ref={wrapper}>
+            <Styles.Title ref={title}>{text}</Styles.Title>
+            <button onClick={CustomAnimationEvent}>click</button>
+        </Styles.Wrapper>)
+};
+
