972997f36f8c074577158c5a09eb5f18af67a821
TKreateDev <tanbir98@hotmail.it>
2022-07-14 13:07:54 +0100

    pretty semi colons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import Introduction from './blocks/Introduction/Introduction';
 
 function App() {
   return (
     <Introduction/>
   );
-}
+};
 
 export default App;
